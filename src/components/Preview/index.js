94ad77ef6c41dcc55ff486e0a43695ff34599f9e
wh1100717 <me@emptystack.net>
2015-09-16 13:39:03 +0800

    update

---
+++ src/components/Preview/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import styles from './Preview.css'
+import withStyles from '../../utils/withStyles'
+
+@withStyles(styles)
+class Preview extends Component {
+
+  render() {
+    return (
+      <div className="Preview">
+        Preview
+      </div>
+    )
+  }
+
+}
+
+export default Preview
